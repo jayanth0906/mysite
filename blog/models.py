e405e50840aece3ec0de3539507b8ba89d3cdbe0
jayanth0906 <jayanthtirupathi12@gmail.com>
2018-09-24 22:57:23 -0500

    adding html files

--- blog/models.py
+++ blog/models.py
@@ -42,3 +42,12 @@ class Meta:
 
 def __str__(self):
     return self.title
+
+
+def get_absolute_url(self):
+    return reverse('blog:post_detail',
+                   args=[self.publish.year,
+                         self.publish.month,
+                         self.publish.day,
+                         self.slug])
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
