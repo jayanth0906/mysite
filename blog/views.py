6bf1edc1589b5a670018412783607b38db03ce46
jayanth0906 <jayanthtirupathi12@gmail.com>
2018-09-24 23:32:09 -0500

    adding pagination files

--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,20 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
