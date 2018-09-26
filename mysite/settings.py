350bc0d55c7ea4a2c5300244fb3b86e02ce8a03b
jayanth0906 <jayanthtirupathi12@gmail.com>
2018-09-26 18:56:01 -0500

    adding email files

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,3 +142,18 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_HOST_USER = 'jayanthtirupathi12@gmail.com'
+
+EMAIL_HOST_PASSWORD = ''
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+
+
