55a6ff7632e9c2ddc4a83094376b7c7fd1fbc3f9
jayanth0906 <jayanthtirupathi12@gmail.com>
2018-09-26 23:28:46 -0500

    adding email files

--- blog/models.py
+++ blog/models.py
@@ -35,19 +35,16 @@ class Post(models.Model):
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
 
+    class Meta:
+        ordering = ('-publish',)
 
-class Meta:
-    ordering = ('-publish',)
+    def __str__(self):
+        return self.title
 
-
-def __str__(self):
-    return self.title
-
-
-def get_absolute_url(self):
-    return reverse('blog:post_detail',
-                   args=[self.publish.year,
-                         self.publish.month,
-                         self.publish.day,
-                         self.slug])
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
